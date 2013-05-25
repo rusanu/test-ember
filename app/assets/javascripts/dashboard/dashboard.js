f969b377fe15c885cbcb948ace4e49da28ff4422
Remus Rusanu <contact@rusanu.com>
2013-05-25 14:15:39 -0400

    initital

---
+++ app/assets/javascripts/dashboard/dashboard.js
@@ -0,0 +1,4 @@
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestEmber::Application.config.secret_token = '5cadad82c02aca5ac1e45dc0a7e8615b9a6abc05d7dddf571b196cffdc7123b5f16244d2a154b743498eee77e5b7bd9a54bd04457ef48265904efe2ac35aa6a4'
